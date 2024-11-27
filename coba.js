e6ab289f866f1f4699f2213191688b06508ca306
Gibun <abgibun@gmail.com>
2024-11-27 15:53:31 +0700

    Test

--- coba.js
+++ coba.js
@@ -1,19 +1,3 @@
 function fungsiSatu() {
   console.log('fungsi satu');
 }
-
-function fungsiDua() {
-  console.log('Fungsi dua');
-}
-
-function fungsiTiga() {
-  console.log('Fungsi tiga');
-}
-
-function fungsiEmpat() {
-  console.log("Fungsi empat");
-}
-
-function fungsiEmpat() {
-  console.log("Fungsi empat");
-}
